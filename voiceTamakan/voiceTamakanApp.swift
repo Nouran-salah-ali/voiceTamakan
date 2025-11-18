e582238c2ffa59768e4d16592302e1dbb9b3b1b8
Nouran-salah-ali <nouransalahali@gmail.com>
2025-11-18 10:46:34 +0300

    Initial Commit

---
+++ voiceTamakan/voiceTamakanApp.swift
@@ -0,0 +1,17 @@
+//
+//  voiceTamakanApp.swift
+//  voiceTamakan
+//
+//  Created by nouransalah on 27/05/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct voiceTamakanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ voiceTamakanTests/voiceTamakanTests.swift
@@ -0,0 +1,17 @@
+//
+//  voiceTamakanTests.swift
+//  voiceTamakanTests
+//
+//  Created by nouransalah on 27/05/1447 AH.
+//
+
+import Testing
+@testable import voiceTamakan
+
+struct voiceTamakanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
